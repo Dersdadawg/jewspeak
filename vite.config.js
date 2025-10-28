d2cb9224c107b37d7216213f6d64960fe8a1a4f1
Anders Christensen <anders@Anderss-MacBook-Air.local>
2025-10-28 14:17:50 +0900

    Fix vite.config.js base path to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jewspeak/', // GitHub repo name
+  base: '/1984hasbara/', // GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
