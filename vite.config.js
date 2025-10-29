3a10a52dbe2cbd711bb39ecaf2e5973c022e26e3
Anders Christensen <andicore100@gmail.com>
2025-10-29 19:37:57 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/1984hasbara/', // GitHub repo name
+  base: '/jewspeak/', // GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
